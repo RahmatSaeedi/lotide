3cd4866bf6e08604bbdd5264a4a8b9230ae6c318
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-06-26 16:12:22 -0600

    head.js: returns the head of an array

---
+++ head.js
@@ -0,0 +1,18 @@
+// Returns the head of an array.
+// Returns 'undefined' for empty array
+// jshint esversion: 6
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  return array[0];
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]) , undefined);
--- readme.md
+++ readme.md
@@ -2,11 +2,21 @@
 It's a simplified & minified version of [Lodash](https://github.com/lodash/lodash) library 
 
 # Contents
-## assertEqual
+## assertEqual.js
  A custom assert function with emoji icons.
 
 Output
  ```bash
   🛑🛑🛑 Assertion Failed: "Lighthouse Labs" !== "Bootcamp"
   ✅✅✅ Assertion Passed: 1 === 1
+ ```
+## head.js
+ Returns the head of an array. Returns 'undefined' for empty array.
+ 
+ ```javascript.js
+head([1, 2, 3]);
+// => 1
+ 
+head([]);
+// => undefined
  ```
