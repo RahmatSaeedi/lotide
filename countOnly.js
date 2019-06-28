4e1d74882dd1fd9aa2cd771bb5288923b1a9cc48
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-06-27 22:24:14 -0600

    return an object containing counts of everything that the input object listed

---
+++ countOnly.js
@@ -0,0 +1,23 @@
+// returns an object containing counts of everything that the input object listed
+// jshint esversion : 6
+
+const countOnly = function(array, itemsToCount) {
+  let out = {};
+
+  for (let key in itemsToCount) {
+    if (itemsToCount[key]) {
+      array.forEach(e => {
+        if (e === key) {
+          if (!out[key]) {
+            out[key] = 1;
+          } else {
+            out[key] ++;
+          }
+        }
+      });
+    }
+  }
+
+  return out;
+};
+
