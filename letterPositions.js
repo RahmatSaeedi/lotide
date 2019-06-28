988f4fe04f93b482ee3a872eeb0931f82e203f92
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-06-28 10:07:16 -0600

    return all the indices in the string where each character is found

---
+++ letterPositions.js
@@ -0,0 +1,16 @@
+// Returns an object, containing all the indices in the string where each character is found. Indicies are zero-based.
+// jshint esversion : 6
+
+const letterPositions = function(str) {
+  let out = {};
+  
+  str.split('').forEach(letter => {
+    out[letter] = [];
+  });
+
+  for (let i = 0; i < str.length; i++) {
+    out[str[i]].push(i);
+  }
+
+  return out;
+};
