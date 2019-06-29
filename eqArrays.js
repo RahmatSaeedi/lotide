20b522cdeac1fc4b80d34bf01c7aec04a628047a
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-06-28 18:18:08 -0600

    Takes in two arrays and returns true or false, based on a perfect match

--- eqArrays.js
+++ eqArrays.js
@@ -1,21 +1,25 @@
 // Takes in two arrays and returns true or false, based on a perfect match.
-// Does not work with nested arrays
 // jshint esversion : 6
 
 const eqArrays = function(array1, array2) {
-  if (array1.length !== array2.length) {
+  if (!(array1 instanceof Array) || !(array2 instanceof Array) || (array1.length !== array2.length)) {
     return false;
   } else {
-    let length = array1.length;
-    while (length > 0) {
-      length --;
-      if (array1[length] !== array2[length]) {
+    let index = array1.length;
+    while (index > 0) {
+      index --;
+      if (array1[index] instanceof Array || array2[index] instanceof Array) {
+        if (!eqArrays(array1[index], array2[index])) {
+          return false;
+        }
+      } else if (array1[index] !== array2[index]) {
         return false;
       }
     }
     return true;
   }
 };
+
 
 
 eqArrays([1, 2, 3], [1, 2, 3]);               // => true
