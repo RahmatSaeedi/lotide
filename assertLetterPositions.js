ad5b5aafc1dc2cfc721aa814a9ad835302551cae
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-06-29 11:56:47 -0600

    Asserts the functionality of letterPositions.js

--- assertLetterPositions.js
+++ assertLetterPositions.js
@@ -2,20 +2,33 @@
 // letterPositions.js: Returns an object, containing all the indices in the string where each character is found. Indicies are zero-based.
 // jshint esversion : 6
 
-const assertArraysEqual = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    console.log(`🛑  Assertion Failed: Lengths don't match, ${array1.length} !== ${array2.length}`);
+const eqArrays = function(array1, array2) {
+  if (!(array1 instanceof Array) || !(array2 instanceof Array) || (array1.length !== array2.length)) {
+    return false;
   } else {
-    let length = array1.length;
-    while (length > 0) {
-      length --;
-      if (array1[length] !== array2[length]) {
-        console.log(`🛑  Assertion Failed: values or types don't match, ${array1[length]} !== ${array2[length]}`);
+    let index = array1.length;
+    while (index > 0) {
+      index --;
+      if (array1[index] instanceof Array || array2[index] instanceof Array) {
+        if (!eqArrays(array1[index], array2[index])) {
+          return false;
+        }
+      } else if (array1[index] !== array2[index]) {
+        return false;
       }
     }
-    console.log(`✅  Assertion Passed: ${array1} === ${array2}`);
+    return true;
   }
 };
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅  Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑  Assertion Failed: values or types don't match, ${array1} !== ${array2}`);
+  }
+};
+
 
 const letterPositions = function(str) {
   let out = {};
