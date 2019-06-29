095de143bd917d89b452a0145fc1992ff391b1e9
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-06-29 11:49:44 -0600

    Take in two arrays, asserts that the two are identical, and `console.log` an appropriate message

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,29 @@
 // Take in two arrays, asserts that the two are identical, and `console.log` an appropriate message
-// Does not work with nested arrays
 // jshint esversion : 6
-const assertArraysEqual = function(array1, array2) {
-  if (array1.length !== array2.length) {
-    console.log(`🛑  Assertion Failed: Lengths don't match, ${array1.length} !== ${array2.length}`);
+const eqArrays = function(array1, array2) {
+  if (!(array1 instanceof Array) || !(array2 instanceof Array) || (array1.length !== array2.length)) {
+    return false;
   } else {
-    let length = array1.length;
-    while (length > 0) {
-      length --;
-      if (array1[length] !== array2[length]) {
-        console.log(`🛑  Assertion Failed: values or types don't match, ${array1[length]} !== ${array2[length]}`);
+    let index = array1.length;
+    while (index > 0) {
+      index --;
+      if (array1[index] instanceof Array || array2[index] instanceof Array) {
+        if (!eqArrays(array1[index], array2[index])) {
+          return false;
+        }
+      } else if (array1[index] !== array2[index]) {
+        return false;
       }
     }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
     console.log(`✅  Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑  Assertion Failed: values or types don't match, ${array1} !== ${array2}`);
   }
 };
 
