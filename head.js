1454e75f21daa5fe0435d40e5ea464146a0ead01
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-06-29 12:02:29 -0600

    Returns the head of an array.

--- head.js
+++ head.js
@@ -3,9 +3,9 @@
 // jshint esversion: 6
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅  Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑  Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
