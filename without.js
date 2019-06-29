ec6b8ad0e3ea507fd12065070e8468070b352113
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-06-29 12:09:26 -0600

    Will return a subset of a given array, removing unwanted elements.

--- without.js
+++ without.js
@@ -1,11 +1,12 @@
 // Will return a subset of a given array, removing unwanted elements.
-// This does not function correctly when using nested arrays or arrays of objects.
 // jshint esversion : 6
 const without = function(sourceArray, itemsToRemoveArray) {
   let newArray = [];
 
   for (let i = 0; i < sourceArray.length; i++) {
-    if (!itemsToRemoveArray.includes(sourceArray[i])) {
+    if (sourceArray[i] instanceof Object) {
+      newArray.push(without(sourceArray, itemsToRemoveArray));
+    } else if (!itemsToRemoveArray.includes(sourceArray[i])) {
       newArray.push(sourceArray[i]);
     }
   }
