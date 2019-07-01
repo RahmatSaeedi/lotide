6ee69fd0691b79693087071882eb8a64f21fbde3
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-07-01 10:03:30 -0600

    Refactored head

--- head.js
+++ head.js
@@ -1,19 +1,11 @@
 // Returns the head of an array.
 // Returns 'undefined' for empty array
 // jshint esversion: 6
-const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   return array[0];
 };
 
-
-head.assertFunctionality = function() {
-  console.log("Testing " + this.name);
-  assertEqual(head([5,6,7]), 5);
-  assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-  assertEqual(head([]) , undefined);
-};
 
 
 module.exports = head;
