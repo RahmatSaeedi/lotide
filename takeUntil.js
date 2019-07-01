052ceb1a854808ccb0036f7cec4425ea157f3e2b
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-07-01 10:20:50 -0600

    Refactored takeUntil

--- takeUntil.js
+++ takeUntil.js
@@ -1,7 +1,5 @@
 // Creates a slice of array with elements taken from the beginning. Elements are taken until predicate returns falsey. The predicate is invoked with three arguments: (value, index, array).
 // jshint esversion : 6
-const assertArraysEqual = require('./eqArrays').assertArraysEqual;
-
 
 const takeUntil = function(array, predicate) {
   if (array instanceof Array && predicate instanceof Function) {
@@ -17,23 +15,5 @@ const takeUntil = function(array, predicate) {
     return [];
   }
 };
-
-
-
-
-takeUntil.assertFunctionality = function() {
-  console.log("Testing " + this.name);
-  
-  const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-  const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-
-  const results1 = takeUntil(data1, x => x < 0);
-  const results2 = takeUntil(data2, x => x === ',');
-
-  assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
-  assertArraysEqual(results2, [ "I've", 'been', 'to', 'Hollywood' ]);
-};
-
-
 
 module.exports = takeUntil;
---
+++ test/takeUntilTest.js
@@ -0,0 +1,22 @@
+// jshint esversion : 6
+const takeUntil = require('../takeUntil');
+const eqArrays = require('../eqArrays');
+
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅  Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑  Assertion Failed: values or types don't match, ${array1} !== ${array2}`);
+  }
+};
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+
+const results1 = takeUntil(data1, x => x < 0);
+const results2 = takeUntil(data2, x => x === ',');
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ "I've", 'been', 'to', 'Hollywood' ]);
