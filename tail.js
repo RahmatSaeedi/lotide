7778e47445757b7f85a1c37746e658a21a98cea5
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-07-01 10:17:39 -0600

    Refactored tail

--- tail.js
+++ tail.js
@@ -1,21 +1,8 @@
 // Returns the tail of an array. ie, everything after the first element.
 // jshint esversion : 6
-const assertEqual = require('./assertEqual');
-
 
 const tail = function(array) {
   return array.slice(1);
 };
-
-tail.assertFunctionality = function() {
-  let x = [1, 2, 3, 4];
-  let i = 2;
-  console.log("Testing " + this.name);
-  tail(x).forEach(a => {
-    assertEqual(a, i++);
-  });
-};
-
-
 
 module.exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,10 @@
+// jshint esversion : 6
+const assertEqual = require('./assertEqual');
+const tail = require('./tail');
+
+
+let x = [1, 2, 3, 4];
+let i = 2;
+tail(x).forEach(a => {
+  assertEqual(a, i++);
+});
