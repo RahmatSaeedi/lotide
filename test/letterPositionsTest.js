8fd0627d16546f85b6b12ce6df55e18326c65dbf
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-07-01 10:29:49 -0600

    fixed require directory ref. error

--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -1,6 +1,6 @@
 // jshint esversion : 6
-const letterPositions = require('./letterPositions');
-const eqArrays = require('./eqArrays');
+const letterPositions = require('../letterPositions');
+const eqArrays = require('../eqArrays');
 
 const assertArraysEqual = function(array1, array2) {
   if (eqArrays(array1, array2)) {
