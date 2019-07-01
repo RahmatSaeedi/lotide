f9be808d502da3f9a3178ebc31f48c637f0f0d27
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-07-01 10:32:36 -0600

    logs to the console if the two arrays are equal

---
+++ test/assertArraysEqualLogger.js
@@ -0,0 +1,12 @@
+// jshint esversion : 6
+const eqArrays = require('../eqArrays');
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅  Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑  Assertion Failed: values or types don't match, ${array1} !== ${array2}`);
+  }
+};
+
+module.exports = assertArraysEqual;
