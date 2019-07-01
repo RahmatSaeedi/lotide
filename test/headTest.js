18cd3929bb5d9d51fcece9493dc72e9246aacf4c
Rahmat Saeedi <RahmatSaeedi@users.noreply.github.com>
2019-07-01 10:29:34 -0600

    fixed require directory ref. error

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,6 @@
 // jshint esversion: 6
-const assertEqual = require('./assertEqual');
-const head = require('./head');
+const assertEqual = require('../assertEqual');
+const head = require('../head');
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
